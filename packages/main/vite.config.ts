be67e7436d874cdef90fc561a4a5171c7415e41c
kimizuy <kimizuyamasaki@gmail.com>
2021-09-26 03:29:50 +0900

    use lib mode

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react'],
+    },
+  },
 })
