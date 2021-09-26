bf78d9b42ebff7c2874cf0f2428af5c401224977
kimizuy <kimizuyamasaki@gmail.com>
2021-09-27 00:46:39 +0900

    add example

---
+++ packages/example/src/App.tsx
@@ -0,0 +1,108 @@
+import './App.css'
+import { Bar, ChartConfig } from '../../main/src'
+import { useState } from 'react'
+
+const config: ChartConfig = {
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+}
+
+const config2: ChartConfig = {
+  data: {
+    labels: ['a', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+}
+
+function App() {
+  const [configState, setConfigState] = useState(config)
+  const [input, setInput] = useState('')
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(e) => {
+          setInput(e.currentTarget.value)
+        }}
+      />
+      <Bar {...configState} />
+      <div>
+        <button
+          onClick={() => {
+            console.log('click!')
+            if (config?.data?.labels?.[0] === configState?.data?.labels?.[0]) {
+              setConfigState(config2)
+              return
+            }
+            setConfigState(config)
+          }}
+        >
+          config2
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
