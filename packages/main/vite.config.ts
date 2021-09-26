89e90e88e1a6d544bbd73aa47068e41b85e601f5
kimizuy <kimizuyamasaki@gmail.com>
2021-09-26 21:58:06 +0900

    add build settings

--- packages/main/vite.config.ts
+++ packages/main/vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src'),
-      formats: ['es'],
+      entry: 'src/index.tsx',
+      name: 'react-chartjs',
     },
     rollupOptions: {
       external: ['react'],
